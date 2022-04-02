1268f3470c75b3256512a31dbdbc65a3b472eaac
Adib Zefar <adibzefar@gmail.com>
2022-04-02 16:39:19 -0400

    Reverse.js added

--- average.js
+++ average.js
@@ -2,9 +2,10 @@ function average(list) {
   var sum = 0;
 
   for (var num of list) {
-    sum ++ num;
+    sum =+ num;
   }
 
   return sum / list.length;
+}
 
 console.log(average([3, 5, 7]));
--- reverse.js
+++ reverse.js
@@ -1,8 +1,9 @@
 var input = process.argv[2];
 
-if (input)
-  console.log(reverse(input));
-
 function reverse(original) {
-  return orignal.split('').reverseList().join('');
+  return original.split('').reverse().join('');
+}
+
+if (input) {
+  console.log(reverse(input));
 }
